ace150dc8c5203c0fe9438c3b580be9724f1bac0
andrewmilligan <andrew.i.milligan@gmail.com>
2017-12-20 11:25:39 -0700

    added VIRTUAL_ENV_DIR to base settings

--- dad_wagtail/dad_wagtail/settings/base.py
+++ dad_wagtail/dad_wagtail/settings/base.py
@@ -18,6 +18,7 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+VIRTUAL_ENV_DIR = os.path.join(os.path.dirname(BASE_DIR), 'env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
