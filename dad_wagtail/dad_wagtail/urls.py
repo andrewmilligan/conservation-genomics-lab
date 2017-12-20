3cff063a5a9bffe93c9814f888bec53aaeba538f
andrewmilligan <andrew.i.milligan@gmail.com>
2017-12-20 09:20:22 -0700

    added static media serving to urls.py; this should not persist for production, but will work for a demo

--- dad_wagtail/dad_wagtail/urls.py
+++ dad_wagtail/dad_wagtail/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     #    url(r'^pages/', include(wagtail_urls)),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     from django.conf.urls.static import static
