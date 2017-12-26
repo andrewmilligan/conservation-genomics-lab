aea2bbebcf680f702acb74424bc9b02b6ac0b3fa
andrewmilligan <andrew.i.milligan@gmail.com>
2017-12-26 13:37:08 -0700

    updated the way that the settings module is set in manage.py for easy toggling

--- manage.py
+++ manage.py
@@ -5,10 +5,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cgri.settings.dev")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-        "cgri.settings.production")
 
-    from django.core.management import execute_from_command_line
+  #settings_module = "cgri.settings.dev"
+  settings_module = "cgri.settings.production"
 
-    execute_from_command_line(sys.argv)
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+      settings_module)
+
+  from django.core.management import execute_from_command_line
+
+  execute_from_command_line(sys.argv)
