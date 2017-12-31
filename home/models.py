777d522e78b7980bfa303ae9bfcb1741e2408650
andrewmilligan <andrew.i.milligan@gmail.com>
2017-12-31 12:52:48 -0500

    added a funder model to the home app that can be used to display funder on the home page

--- home/models.py
+++ home/models.py
@@ -2,12 +2,15 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
+from modelcluster.fields import ParentalKey
+
 from core.models import LabPage, HighlightPage
+from articles.models import ProjectPage, PublicationPage, PersonPage
 
 ## HomePage
 #
@@ -20,17 +23,44 @@ class HomePage(LabPage):
       'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+',
       null=True
       )
+  secondary_image = models.ForeignKey(
+      'wagtailimages.Image', on_delete=models.SET_NULL, related_name='+',
+      null=True, blank=True
+      )
 
   content_panels = Page.content_panels + [
       FieldPanel('brief_title'),
       FieldPanel('blurb', classname="full"),
       ImageChooserPanel('image'),
+      ImageChooserPanel('secondary_image'),
+      InlinePanel('funders', label="Funders"),
       ]
 
   def get_context(self, request):
     # Update context to include project pages to highlight
     context = super(HomePage, self).get_context(request)
-    projects = Page.objects.type(HighlightPage).live()
+    projects = Page.objects.type(ProjectPage).live()
+    people = Page.objects.type(ProjectPage).live()
+    publications = Page.objects.type(PublicationPage).live()
     context['projects'] = projects
+    context['people'] = people
+    context['publications'] = publications
     return context
 
+
+## HomePageFunder
+#
+#  A funder to be displayed on the home page.
+#
+class HomePageFunder(Orderable):
+  page = ParentalKey(HomePage, related_name='funders')
+  logo = models.ForeignKey(
+      'wagtailimages.Image', on_delete=models.CASCADE, related_name='+',
+      null=True
+      )
+  link = models.URLField(null=True, blank=True)
+
+  panels = [
+      ImageChooserPanel('logo'),
+      FieldPanel('link'),
+      ]
