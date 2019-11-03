2a7a22ab8a40fbe13263af4a1868afdf676d23c7
Andrew Milligan <andrew.i.milligan@gmail.com>
2019-11-03 12:07:46 -0500

    styling for CGRI styles (light/dark) etc

--- freestyle/blocks/__init__.py
+++ freestyle/blocks/__init__.py
@@ -1,11 +1,7 @@
 from .cgri_style_block import CGRIStyleBlock # noqa
 from .carousel_block import CarouselBlock # noqa
 from .carousel_image_block import CarouselImageBlock # noqa
-from .current_projects_block import CurrentProjectsBlock # noqa
 from .image_link_block import ImageLinkBlock # noqa
-from .image_link_columns_block import ImageLinkColumnsBlock # noqa
-from .recent_publications_block import RecentPublicationsBlock # noqa
 from .text_column_block import TextColumnBlock # noqa
-from .text_columns_block import TextColumnsBlock # noqa
 from .page_index_block import PageIndexBlock # noqa
 from .columns_block import ColumnsBlock # noqa
--- freestyle/templatetags/freestyle_tags.py
+++ freestyle/templatetags/freestyle_tags.py
@@ -9,62 +9,67 @@ register = template.Library()
 
 @register.inclusion_tag("freestyle/blocks/component_title.html")
 def cgri_component_title(title):
-  return {
-    'title': title
-  }
+    return {
+        'title': title
+    }
 
 
 @register.simple_tag
 def cgri_column_class(n):
-  if 12 % n == 0:
-    return f'col-md-{int(12 / n)}'
-  else:
-    return 'col-md-2'
+    if 12 % n == 0:
+        return f'col-md-{int(12 / n)}'
+    else:
+        return 'col-md-2'
+
+
+@register.simple_tag
+def cgri_style_class(style="light"):
+        return f"cgri-style cgri-style-{style}"
 
 
 @register.inclusion_tag("freestyle/carousel.html", takes_context=True)
 def cgri_carousel(context):
-  '''
-  This template tag is used to build the home page carousel.
-  '''
-  page = context['page']
-  projects = ProjectPage.objects.live().filter(promoted=True)
-  return {
-    'page': page,
-    'projects': projects,
-  }
+    '''
+    This template tag is used to build the home page carousel.
+    '''
+    page = context['page']
+    projects = ProjectPage.objects.live().filter(promoted=True)
+    return {
+        'page': page,
+        'projects': projects,
+    }
 
 
 @register.inclusion_tag("freestyle/funders.html", takes_context=True)
 def cgri_funders(context):
-  '''
-  This template tag is used to build the home page funder section.
-  '''
-  homepage = context['request'].site.root_page.specific
-  funders = homepage.funders.all()
-  primary_funders = funders[:3]
-  other_funders = funders[3:]
-  return {
-    'funders': funders,
-    'primary_funders': primary_funders,
-    'other_funders': other_funders,
-  }
+    '''
+    This template tag is used to build the home page funder section.
+    '''
+    homepage = context['request'].site.root_page.specific
+    funders = homepage.funders.all()
+    primary_funders = funders[:3]
+    other_funders = funders[3:]
+    return {
+        'funders': funders,
+        'primary_funders': primary_funders,
+        'other_funders': other_funders,
+    }
 
 
 @register.inclusion_tag("freestyle/recent_publications.html")
 def cgri_recent_publications():
-  '''
-  This template tag is used to build the home page recent publications
-  section.
-  '''
-  publications = PublicationPage.objects.live().order_by("-date")
-  return {'publications': publications}
+    '''
+    This template tag is used to build the home page recent publications
+    section.
+    '''
+    publications = PublicationPage.objects.live().order_by("-date")
+    return {'publications': publications}
 
 
 @register.inclusion_tag("freestyle/current_projects.html")
 def cgri_current_projects():
-  '''
-  This template tag is used to build the home page current projects section
-  '''
-  projects = ProjectPage.objects.live().filter(promoted=True)
-  return {'projects': projects}
+    '''
+    This template tag is used to build the home page current projects section
+    '''
+    projects = ProjectPage.objects.live().filter(promoted=True)
+    return {'projects': projects}
